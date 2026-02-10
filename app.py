d6a89967cbeef296fff173f75165d9fe0a3293d3
root <root@ip-172-31-79-46.ec2.internal>
2026-02-10 17:55:08 +0000

    change commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Real-Time DevOps Project 🚀"
+    return "Hello from AUTO DEPLOYED DevOps Pipeline 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
